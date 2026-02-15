cc6ba06688e5dfd294036876c5e6566f9c327a77
kevbennett <kevin.bennett0908@gmail.com>
2026-02-15 18:05:43 -0500

    Initial ecommerce project with products app

---
+++ products/models.py
@@ -0,0 +1,29 @@
+import datetime
+from django.db import models
+from django.utils import timezone
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description= models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT,
+                                 related_name='products')
+    image = models.ImageField(upload_to='products/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
