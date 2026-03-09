0405a7c38b8aff73055f10802e461513e96d9769
kevbennett <kevin.bennett0908@gmail.com>
2026-03-08 21:26:58 -0400

    Fixed settings.py

--- canadian_catalog/settings.py
+++ canadian_catalog/settings.py
@@ -143,12 +143,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
 }
-<<<<<<< HEAD
 
 # Stripe
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 STRIPE_WEBHOOK_SECRET = os.environ.get('STRIPE_WEBHOOK_SECRET')
 STRIPE_CURRENCY = 'cad'
-=======
->>>>>>> 3334e8a579b79091e8a0c5885998cb91548e0b34
